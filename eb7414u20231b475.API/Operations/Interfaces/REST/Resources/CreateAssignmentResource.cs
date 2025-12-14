2550c4f3ca309a59527e5de1ec1b1aa96ba1876a
Trevor <valentinosolischang@gmail.com>
2025-12-13 19:17:16 -0500

    Add CreateAssignmentResource record to represent assignment creation data

---
+++ eb7414u20231b475.API/Operations/Interfaces/REST/Resources/CreateAssignmentResource.cs
@@ -0,0 +1,9 @@
+namespace eb7414u20231b475.API.Operations.Interfaces.REST.Resources
+{
+    /// <summary>
+    /// Represents the resource to create an assignment
+    /// </summary>
+    public record CreateAssignmentResource(int StudentId)
+    {
+    }
+}
